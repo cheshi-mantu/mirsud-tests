d3a3f9c1f1cadfcb187e90dbf83661185b85dc2e
egorivanov <egor.ivanov@pm.me>
2021-10-30 12:09:59 +0300

    updated steps description

--- src/test/java/tests/CheckingLawSuitAvailability.java
+++ src/test/java/tests/CheckingLawSuitAvailability.java
@@ -13,7 +13,6 @@ public class CheckingLawSuitAvailability extends TestBase {
         step("Открываем страницу с рассмотрением дел", () -> {
             open(hostname);
         });
-
         step("Вызываем первый выпадающий список с типами дел и выбираем гражданские дела", () -> {
             $(".fancy-select",0).click();
             $(by("data-raw-value", "civil")).click();
@@ -22,10 +21,9 @@ public class CheckingLawSuitAvailability extends TestBase {
             $("div.full-name").click();
             $(":focus").val(nameToSearch).pressEnter();
         });
-        step("Клик на поле ФИО и заполняем фамилией "+ nameToSearch, () -> {
+        step("Проверяем, что 'Судебные дела, удовлетворяющие запросу, не найдены'", () -> {
             $("div.hearings").shouldHave(text("Судебные дела, удовлетворяющие запросу, не найдены"));
         });
-
 
     }
 
