ae85f86ca2b31dc39c4e5c4ebe8a489b4053ecad
egorivanov <egor.ivanov@pm.me>
2021-10-30 11:50:45 +0300

    Initial commit

---
+++ src/test/java/tests/CheckingLawSuitAvailability.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class CheckingLawSuitAvailability extends TestBase {
+    @Test
+    void successfulFillFormTest() {
+        step("Открываем страницу с рассмотрением дел", () -> {
+            open(hostname);
+        });
+
+        step("Вызываем первый выпадающий список с типами дел и выбираем гражданские дела", () -> {
+            $(".fancy-select",0).click();
+            $(by("data-raw-value", "civil")).click();
+        });
+        step("Клик на поле ФИО и заполняем фамилией "+ nameToSearch, () -> {
+            $("div.full-name").click();
+            $(":focus").val(nameToSearch).pressEnter();
+        });
+        step("Клик на поле ФИО и заполняем фамилией "+ nameToSearch, () -> {
+            $("div.hearings").shouldHave(text("Судебные дела, удовлетворяющие запросу, не найдены"));
+        });
+
+
+    }
+
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+
+public class TestBase {
+    static final String nameToSearch = System.getProperty("search_name", "Северный город");
+    static final String hostname = System.getProperty("host_name","https://mirsud.spb.ru/cases/");
+
+    @BeforeAll
+    static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.startMaximized = true;
+        Configuration.timeout = 15000;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("enableVNC", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = System.getProperty("remote_driver_url", "http://192.168.1.123:4444/wd/hub/");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Latest screenshot");
+        closeWebDriver();
+    }
+
+}
